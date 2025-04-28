7a61484f03cd4adcf8fdb1de53fb91c34fb8948f
Nk121212 <setiawankahfii@gmail.com>
2025-04-28 14:38:18 +0700

    sederhanakan login.php

--- api/backend/login.php
+++ api/backend/login.php
@@ -1,31 +1,32 @@
 <?php
 
-var_dump($_SERVER['REQUEST_METHOD']);
+    var_dump($_SERVER['REQUEST_METHOD']);
+    exit;
 
-header('Content-Type: application/json');
+    // header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Proses data login di sini
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
+    // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //     // Proses data login di sini
+    //     $username = $_POST['username'] ?? '';
+    //     $password = $_POST['password'] ?? '';
 
-    // ... logika autentikasi ...
+    //     // ... logika autentikasi ...
 
-    $response = ['status' => 'success', 'token' => generateRandomString(32)];
-    echo json_encode($response);
-} else {
-    // Jika bukan metode POST, kirim error 405
-    http_response_code(405);
-    echo json_encode(['error' => 'Method Not Allowed']);
-}
+    //     $response = ['status' => 'success', 'token' => generateRandomString(32)];
+    //     echo json_encode($response);
+    // } else {
+    //     // Jika bukan metode POST, kirim error 405
+    //     http_response_code(405);
+    //     echo json_encode(['error' => 'Method Not Allowed']);
+    // }
 
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
+    // function generateRandomString($length = 10) {
+    //     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    //     $charactersLength = strlen($characters);
+    //     $randomString = '';
+    //     for ($i = 0; $i < $length; $i++) {
+    //         $randomString .= $characters[rand(0, $charactersLength - 1)];
+    //     }
+    //     return $randomString;
+    // }
 ?>
